75a4253918598c78e0a7747ff588d183dccd30d3
Durlove-joy <joysarker21005@gmail.com>
2025-04-17 02:58:01 +0600

    modified search

---
+++ config.js
@@ -0,0 +1,7 @@
+// This file will be replaced with actual values during deployment
+const config = {
+    OPENCAGE_API_KEY: '33bc15e34bc44677836917ce771d2a55',
+    SHEETDB_API_KEY: 'gk4ag43gyg19l',
+    GOOGLE_SHEETS_API_URL: 'https://script.google.com/macros/s/AKfycbwmcens5pwcDr1SiiVPhJX9itVRTmkQG4l_7-hN4SRmTDCQplrjVQ6yyUFm0L3qlGUz/exec'
+}; 
+ 
