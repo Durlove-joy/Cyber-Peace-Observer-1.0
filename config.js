1ee60c5b9502a205fc812a7ea3dd48400e071a11
Durlove-joy <joysarker21005@gmail.com>
2025-04-18 14:00:54 +0600

    phone version

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This file will be replaced with actual values during deployment
 const config = {
-    fee51db4df654e2f893355bd52a3245f: '33bc15e34bc44677836917ce771d2a55',
+    OPEN_CAGE_API_KEY: '33bc15e34bc44677836917ce771d2a55',
     SHEETDB_API_KEY: 'gk4ag43gyg19l',
     GOOGLE_SHEETS_API_URL: 'https://script.google.com/macros/s/AKfycbwmcens5pwcDr1SiiVPhJX9itVRTmkQG4l_7-hN4SRmTDCQplrjVQ6yyUFm0L3qlGUz/exec'
 }; 
