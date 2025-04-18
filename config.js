eb06c4d711cfc61fab2bff721b134af6c003ef9c
Durlove-joy <joysarker21005@gmail.com>
2025-04-19 04:23:11 +0600

    modified search and title background

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This file will be replaced with actual values during deployment
 const config = {
-    OPEN_CAGE_API_KEY: '33bc15e34bc44677836917ce771d2a55',
+    OPEN_CAGE_API_KEY: 'c21d5ecb21874719841019002dc36760',
     SHEETDB_API_KEY: 'gk4ag43gyg19l',
     GOOGLE_SHEETS_API_URL: 'https://script.google.com/macros/s/AKfycbwmcens5pwcDr1SiiVPhJX9itVRTmkQG4l_7-hN4SRmTDCQplrjVQ6yyUFm0L3qlGUz/exec'
 }; 
